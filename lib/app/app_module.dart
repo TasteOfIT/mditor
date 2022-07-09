7e79f8d774af32689d44c5735f786719729651c5
Le Xing <xingled@gmail.com>
2022-07-09 09:12:09 +0800

    [Le] add dep joplin_database

--- lib/app/app_module.dart
+++ lib/app/app_module.dart
@@ -1,8 +1,14 @@
 import 'package:flutter_modular/flutter_modular.dart';
+import 'package:joplin_database/joplin_database.dart';
 
 import 'app_routes.dart';
 
 class AppModule extends Module {
+  @override
+  List<Module> get imports => [
+        DatabaseModule(),
+      ];
+
   @override
   List<Bind<Object>> get binds => [];
 
